85f4e2eedb0c9e3807dba72776d38c9d936184bc
hasantayyar <tayyar.besik@gmail.com>
2014-09-30 14:03:49 +0300

    compleded #1

--- example.php
+++ example.php
@@ -4,3 +4,5 @@ namespace Hasantayyar\DoiTools;
 
 include __DIR__ . '/vendor/autoload.php';
 
+$doiTools = new DoiTools();
+echo $doiTools->isReal('10.1186%2F1752-0509-4-132');
--- src/Hasantayyar/DoiTools/DoiTools.php
+++ src/Hasantayyar/DoiTools/DoiTools.php
@@ -2,12 +2,40 @@
 
 namespace Hasantayyar\DoiTools;
 
+use GuzzleHttp\Event\ErrorEvent;
+use GuzzleHttp\Client;
 
 /**
  * @author Hasan Tayyar BEŞİK <tayyar.besik@gmail.com>
  * @package DoiTools 
-*/
+ */
 class DoiTools {
 
+    private $baseUrl = 'http://dx.doi.org/';
+    private $client;
+
+//curl -i 'http://dx.doi.org/' -H 'Pragma: no-cache' -H 'Origin: http://dx.doi.org' --data 'hdl=10.1186%2F1752-0509-4-132' 
+
+    public function __construct() {
+        $this->client = new Client(array('defaults' => array('allow_redirects' => false)));
+        $emitter = $this->client->getEmitter();
+        $emitter->on('error', function (ErrorEvent $event) {
+            $event->stopPropagation();
+        });
+    }
+
+    /**
+     * Check if given doi number registered by returning redirected url or false
+     * @param string $doiNumber
+     * @return boolean|string
+     */
+    public function isReal($doiNumber) {
+        $url = $this->baseUrl . $doiNumber;
+        $response = $this->client->get($url);
+        if ($response->getStatusCode() === '303') {
+            return $response->getHeader('Location');
+        }
+        return false;
+    }
 
 }
